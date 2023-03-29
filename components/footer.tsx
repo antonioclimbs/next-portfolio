da23c2f4bf10511cdac18600e3b3c674e07c08ae
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-03-29 10:26:24 -0700

    adding footer icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,6 +3,10 @@ import {
   GitHubIcon,
 } from 'components/icons';
 
+import { GoMarkGithub } from "react-icons/go";
+import { FiMail } from "react-icons/fi"
+import { BsInstagram, BsLinkedin } from "react-icons/bs"
+
 export default function Footer() {
   return (
     <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
@@ -13,8 +17,19 @@ export default function Footer() {
           href="https://github.com/antonioclimbs"
           className="flex items-center gap-2"
         >
-          <GitHubIcon />
-          <p className="h-7">checkout my github</p>
+          <GoMarkGithub size={20} />
+          {/* <p className="h-7">checkout my github</p> */}
+        </a>
+      </li>
+      <li>
+        <a
+          className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/antonio-jesus-ayala/"
+        >
+          <BsLinkedin size={20} />
+          {/* <p className="h-7">email me</p> */}
         </a>
       </li>
       <li>
@@ -24,8 +39,8 @@ export default function Footer() {
           target="_blank"
           href="https://www.instagram.com/ayala.photographs/"
         >
-          <ArrowIcon />
-          <p className="h-7">follow me on instagram</p>
+          <BsInstagram size={21} />
+          {/* <p className="h-7">follow me on instagram</p> */}
         </a>
       </li>
       <li>
@@ -35,8 +50,8 @@ export default function Footer() {
           target="_blank"
           href="mailto:ayala.antonio.jesus@gmail.com"
         >
-          <ArrowIcon />
-          <p className="h-7">email me</p>
+          <FiMail size={22} />
+          {/* <p className="h-7">email me</p> */}
         </a>
       </li>
     </ul>
