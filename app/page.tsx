397448b2d8ad86c1de66381be0196da48c485013
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-03-29 10:55:05 -0700

    formatting continued for home page

--- app/page.tsx
+++ app/page.tsx
@@ -26,28 +26,28 @@ export default async function HomePage() {
 
   return (
     <section>
-      <p className="max-w-[600px] text-xl pb-3 text-neutral-800 dark:text-neutral-200">
+      <p className="max-w-[600px] text-xl pb-3 text-rose-800 dark:text-rose-100">
         Hi, my name is
       </p>
       <h1 className="font-bold text-5xl font-serif">{name}</h1>
-      <h2 className="font-bold text-2xl mb-3 mt-4 max-w-[600px] text-neutral-800 dark:text-neutral-200">
+      <h2 className="font-bold text-2xl mb-3 mt-4 max-w-[600px] text-orange-800 dark:text-orange-200">
         {tagline()}
       </h2>
-      <h3 className="font-bold my-3 max-w-[460px] text-neutral-800 dark:text-neutral-200">
+      <h3 className="font-bold my-3 max-w-[460px] text-xl text-orange-800 dark:text-orange-100">
         {about()}
       </h3>
-      <p className="my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200">
+      <p className="my-5 w-full text-xl text-rose-800 dark:text-rose-100">
         {bio()}
       </p>
       <div className='text-xl text-black font-medium'>
         <Link href='/about'>
-          <button className='mr-5 border-solid border-2 border-orange-100 bg-orange-100 p-5 px-8 rounded'>About</button>
+          <button className='mr-5 border-solid border-2 border-rose-100 bg-rose-100 p-5 px-8 rounded'>About</button>
         </Link>
         <Link href='/experience'>
-          <button className='m-5 border-solid border-2 border-orange-100 bg-orange-100 p-5 px-8 rounded'>Experience</button>
+          <button className='m-5 border-solid border-2 border-rose-100 bg-rose-100 p-5 px-8 rounded'>Experience</button>
         </Link>
         <Link href='/contact'>
-          <button className='m-5 border-solid border-2 border-orange-100 bg-orange-100 p-5 px-8 rounded'>Contact</button>
+          <button className='m-5 border-solid border-2 border-rose-100 bg-rose-100 p-5 px-8 rounded'>Contact</button>
         </Link>
       </div>
     </section>
