188bb7b23037a598f43c1f137805fce26f158743
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-04-01 14:55:40 -0700

    deleted package lock json

--- components/job.tsx
+++ components/job.tsx
@@ -3,19 +3,19 @@ interface JobProps {
   description: string[]
 }
 
-export default function Job(props: JobProps): JSX.Element {
+export default function Job(props: JobProps) {
   const { job, description } = props;
-  const bullets = [] as JSX.Element[];
+  // const bullets = [] as JSX.Element[];
   for (let des in description) {
-    bullets.push(
-      <li>{des}</li>
-    )
+    // bullets.push(
+    //   <li>{des}</li>
+    // )
   }
   return (
     <section>
       <h1>{job}</h1>
       <ul>
-        {bullets}
+        {/* {bullets} */}
       </ul>
     </section>
   );
