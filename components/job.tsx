3d35071132680264dac057d059356db2c77133d8
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-04-03 19:36:16 -0700

    filled in some job descriptions and styled

--- components/job.tsx
+++ components/job.tsx
@@ -1,13 +1,14 @@
 interface JobProps {
-  job: string
+  job: string,
+  title: string,
   description: string[]
 }
 
 export default function Job(props: JobProps): JSX.Element {
-  const { job, description } = props
+  const { job, description, title } = props
   const bullets: JSX.Element[] = description.map((des) => {
     return (
-      <h3 className="font-bold my-3 max-w-[460px] text-xl text-orange-800 dark:text-orange-100">
+      <h3 className="font-bold my-3 text-l text-orange-800 dark:text-orange-100">
         {des}
       </h3>
     )
@@ -22,7 +23,13 @@ export default function Job(props: JobProps): JSX.Element {
   // need to loop through all bullets of description and push them into bullets array as list elements
   return (
     <section className="mb-10">
-      <h2 className="font-bold text-2xl mb-3 mt-4 max-w-[600px] text-orange-800 dark:text-orange-200">{job}</h2>
+      <div className="grid grid-flow-col ">
+        <h2 className="font-bold text-3xl mb-3 mt-4 w-flex text-orange-800 dark:text-orange-200">{job}</h2>
+        <span className="font-bold text-2xl mb-3 mt-5 w-full text-right dark:text-white-800">{title}</span>
+      </div>
+      {/* <span>
+        {title}
+      </span> */}
       <ul className="">
         {bullets}
       </ul>
